e6ea843cd0b08f49f2125facaa50c67b67e0fa71
satyaswarup140 <dsatyaswarup@gmail.com>
2024-07-25 15:20:00 +0000

    solution

---
+++ src/components/LangCommitCountPie/index.js
@@ -0,0 +1,54 @@
+import {PieChart, Pie, Legend, Cell, ResponsiveContainer} from 'recharts'
+
+const COLORS = [
+  '#0088FE',
+  '#00C49F',
+  '#FFBB28',
+  'purple',
+  'Pink',
+  'Orange',
+  'red',
+]
+
+{
+  data.map((entry, index) => (
+    <Cell key={data.name + data.value} fill={COLORS[index % COLORS.length]} />
+  ))
+}
+
+const LanguageCommitCountPie = props => {
+  const {langCommitCount} = props
+  const data = langCommitCount
+
+  return (
+    <ResponsiveContainer width="100%" height={200}>
+      <PieChart>
+        <Pie
+          cx="50%"
+          cy="50%"
+          data={data}
+          startAngle={0}
+          endAngle={360}
+          innerRadius="40%"
+          outerRadius="70%"
+          dataKey="value"
+        >
+          {data.map((entry, index) => (
+            <Cell
+              key={data.name + data.value}
+              fill={COLORS[index % COLORS.length]}
+            />
+          ))}
+        </Pie>
+        <Legend
+          iconType="square"
+          layout="vertical"
+          verticalAlign="middle"
+          align="right"
+        />
+      </PieChart>
+    </ResponsiveContainer>
+  )
+}
+
+export default LanguageCommitCountPie
